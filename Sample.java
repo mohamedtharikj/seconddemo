2918c3801c565e5386051a815ecba64346ec7905
mohamed <tharikt720@gmail.com>
2022-05-11 09:58:02 -0700

    Done

---
+++ Sample.java
@@ -0,0 +1,22 @@
+package Org.Sample;
+
+public class Sample {
+	
+	
+	
+	private void employeeId() {
+		System.out.println("the employee id is 8055");
+		
+
+	}
+	private void employeeName() {
+		System.out.println("employee name is tharik");
+
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.employeeId();
+		s.employeeName();
+	}
+	
+}
